94aedff446b4cf19c7c529dadfb433e66ece0f7a
Rezwan Atcha <rez_a@outlook.com>
2022-06-16 17:14:11 -0400

    tested the api calls with giphy api to make sure the code for api calls is working

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -1,34 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 
-const Searchbar = () => {
+function Searchbar() {
   const [input, setInput] = useState('');
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState([]);
 
   const handleChange = (event) => {
-    const userValue = event.target.value;
-    setInput(userValue);
-    console.log(userValue);
+    const userInput = event.target.value;
+    setInput(userInput);
   };
 
-  const handleClick = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-
-    console.log('Input:', input);
-
-    setInput('');
+    setQuery(input);
   };
+
+  // When the query state changes ([query]), we then run the effect
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=Z60bAfNVYHWngrK2POOW9P9vHXqvPk3B&q=${query}&limit=25&offset=0&rating=g&lang=en`
+        );
+        const json = await response.json();
+        console.log({ json });
+        setResults(
+          json.data.map((item) => {
+            return item.images.preview.mp4;
+          })
+        );
+      } catch (error) {}
+    }
+
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
 
   return (
-    <div>
-      <input
-        type='text'
-        placeholder='Enter something'
-        onChange={handleChange}
-        value={input}
-      />
+    <Fragment>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type='text'
+            placeholder='Enter something'
+            onChange={handleChange}
+            value={input}
+          />
 
-      <button onClick={handleClick}>Submit</button>
-    </div>
+          <button type='submit'>Submit</button>
+        </form>
+        <br />
+        {results.map((item) => (
+          <video autoPlay loop key={item} src={item} />
+        ))}
+      </div>
+    </Fragment>
   );
-};
+}
 
 export default Searchbar;
